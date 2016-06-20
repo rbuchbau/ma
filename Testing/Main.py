aa790907eb253ee19ec8273866194a0b19d2861c
zexe <zexe@email.com>
2016-06-20 13:44:38 +0200

    imports

--- Testing/Main.py
+++ Testing/Main.py
@@ -1,6 +1,12 @@
+# this code comes from the python binding tutorial:
+# http://nbviewer.jupyter.org/github/BVLC/caffe/blob/master/examples/00-classification.ipynb
+
+import numpy as np
+import sys
+import caffe
 
 def main():
-    print "a"
+
 
 if __name__ == '__main__':
     main()
