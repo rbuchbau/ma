9b946c54d40c63fa35395b596f0d444d5134e8cd
zexe <zexe@email.com>
2017-03-30 08:12:09 +0200

    30.3.2017 2

--- caffe_feature_extraction/Main.py
+++ caffe_feature_extraction/Main.py
@@ -33,7 +33,7 @@ def svm(models, features):
     # Fe.convert_binaryproto_to_npy(model)
 
     #train svm
-    # train_svm(models, features, kernel)
+    train_svm(models, features, kernel)
 
     # use svm
     use_svm(models, features)
@@ -84,21 +84,6 @@ def train_svm(models, features, kernel):
 
 # def use_svm(model, features, kernel):
 def use_svm(models, features):
-    # for feature in features:
-    #     acc = []
-    #     # info = feature + '_' + kernel + '_' + duration + '_' + str(svm_size)
-    #     # svm_path = 'svms/' + feature + '/' + kernel + '/svm_' + kernel + '_' + filename + '_' \
-    #     #            + str(svm_size) + '/svm_' + kernel + '_' + filename + '_' + str(svm_size) + '.pkl'
-    #
-    #     filename = '' + model + '_' + feature
-    #     svm_path = 'svms/' + filename + '.pkl'
-    #     # for v in videos:
-    #         # use svm
-    #     acc_values = Fe.load_and_use_svm(svm_path, model, feature, False)
-    #     FileIO.write_accuracy('acc_results/' + filename + '_svm.csv', acc_values)
-    #     acc.append(acc_values[:])
-    #     avg_acc_values = Fe.calc_average_accuracy(acc)
-    #     FileIO.write_accuracy('acc_results_avgs/results.csv', avg_acc_values, info)
 
     groundtruth_path = '../groundtruth/'
 
@@ -117,28 +102,6 @@ def use_svm(models, features):
         for feature in features:
             acc_values_for_all_concepts = Fe.load_and_use_svm(model, feature, all_infos, mapp, True)
             FileIO.write_accuracies(acc_values_for_all_concepts, model + '_' + feature)
-
-
-# def load_and_use_model_svm(model, feature, all_infos, mapp):
-    # acc_values = []
-    # feat_vectors = []
-    # (conceptsList, conceptsList_all, videofiles, needed_shots, shot_paths) = all_infos
-    #
-    # #init caffe net
-    # net, transformer = Fe.init_caffe_net(model)
-    # #read images to classify from folder
-    # print "Preparing images for model " + model
-    # all_images = Fe.load_images_to_classify(transformer, shot_paths)
-    #
-    # # perform classification
-    # print "Classifying for model " + model
-    # predicted_labels = Fe.classify_for_svm(net, all_images, feature, feat_vectors)
-    #
-    # print "Calculating evaluation parameters for model " + model
-    # acc_values_for_all_concepts = Fe.calc_accuracy(predicted_labels, model, conceptsList, mapp)
-    # FileIO.write_accuracies(acc_values_for_all_concepts, model)
-    #
-    # return acc_values
 
 
 
